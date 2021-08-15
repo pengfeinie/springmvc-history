eac34988924eb9dbd8bebd3b984dda6c806a073e
pfnie <pfnie@yahoo.com>
2021-08-15 17:19:09 +0800

    init

---
+++ servlet3.0-helloworld/src/main/java/org/example/HelloWorldServlet.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+
+@WebServlet(name = "helloWorldServlet",value = {"/v1/hello/world"})
+public class HelloWorldServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String name = req.getParameter("name");
+        resp.getOutputStream().write(name.getBytes(StandardCharsets.UTF_8));
+    }
+}
