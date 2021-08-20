fc7c4178a1876fa7b22e367e6ab0c54cbe5042d2
pfnie <pfnie@yahoo.com>
2021-08-20 13:45:17 +0800

    1-2

---
+++ servlet3.0-springmvc-helloworld1_2/src/main/java/org/example/MyWebApplicationInitializer.java
@@ -0,0 +1,20 @@
+package org.example;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.XmlWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+import javax.servlet.ServletContext;
+import javax.servlet.ServletRegistration;
+
+public class MyWebApplicationInitializer implements WebApplicationInitializer {
+
+    @Override
+    public void onStartup(ServletContext servletContext) {
+        XmlWebApplicationContext xmlApplicationContext = new XmlWebApplicationContext();
+        xmlApplicationContext.setConfigLocation("classpath:applicationContext.xml");
+        DispatcherServlet dispatcherServlet = new DispatcherServlet(xmlApplicationContext);
+        ServletRegistration.Dynamic registration = servletContext.addServlet("dispatcherServlet", dispatcherServlet);
+        registration.setLoadOnStartup(1);
+        registration.addMapping("/");
+    }
+}
