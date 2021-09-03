7781dd4aec90942d1c83e557144293a511678871
pfnie <pfnie@yahoo.com>
2021-09-03 13:39:52 +0800

    do

--- servlet3.0-helloworld/src/main/java/org/example/HelloWorldServlet.java
+++ servlet3.0-helloworld/src/main/java/org/example/HelloWorldServlet.java
@@ -6,7 +6,6 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.nio.charset.StandardCharsets;
 
 @WebServlet(name = "helloWorldServlet",value = {"/v1/hello/world"})
 public class HelloWorldServlet extends HttpServlet {
@@ -16,7 +15,7 @@ public class HelloWorldServlet extends HttpServlet {
 	@Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String name = req.getParameter("name");
-        resp.getOutputStream().write(name.getBytes(StandardCharsets.UTF_8));
+        resp.getOutputStream().write(name.getBytes());
     }
 }
 
