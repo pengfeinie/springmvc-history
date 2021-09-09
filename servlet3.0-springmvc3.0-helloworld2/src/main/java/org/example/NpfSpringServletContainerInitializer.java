cc1404a8261875982137bfb02b7f1cfb4a6be4e5
pfnie <pfnie@yahoo.com>
2021-09-09 17:43:14 +0800

    d

--- servlet3.0-springmvc3.0-helloworld2/src/main/java/org/example/NpfSpringServletContainerInitializer.java
+++ servlet3.0-springmvc3.0-helloworld2/src/main/java/org/example/NpfSpringServletContainerInitializer.java
@@ -11,17 +11,16 @@ import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.HandlesTypes;
 
-
 @HandlesTypes(value= {NpfWebApplicationInitializer.class})
 public class NpfSpringServletContainerInitializer implements ServletContainerInitializer {
 
 	
 	public void onStartup(Set<Class<?>> npfWebApplicationInitializer, ServletContext servletContext)throws ServletException{
 		List<NpfWebApplicationInitializer> initializers = new LinkedList<NpfWebApplicationInitializer>();
-
 		if (npfWebApplicationInitializer != null) {
 			for (Class<?> waiClass : npfWebApplicationInitializer) {
-				if (!waiClass.isInterface() && !Modifier.isAbstract(waiClass.getModifiers()) && NpfWebApplicationInitializer.class.isAssignableFrom(waiClass)) {
+				if (!waiClass.isInterface() && !Modifier.isAbstract(waiClass.getModifiers()) && 
+						NpfWebApplicationInitializer.class.isAssignableFrom(waiClass)) {
 					try {
 						initializers.add((NpfWebApplicationInitializer) waiClass.newInstance());
 					}
@@ -31,15 +30,12 @@ public class NpfSpringServletContainerInitializer implements ServletContainerIni
 				}
 			}
 		}
-
 		if (initializers.isEmpty()) {
 			servletContext.log("No Spring WebApplicationInitializer types detected on classpath");
 			return;
 		}
-
 		Collections.sort(initializers, new AnnotationAwareOrderComparator());
 		servletContext.log("Spring WebApplicationInitializers detected on classpath: " + initializers);
-
 		for (NpfWebApplicationInitializer initializer : initializers) {
 			initializer.onStartup(servletContext);
 		}
