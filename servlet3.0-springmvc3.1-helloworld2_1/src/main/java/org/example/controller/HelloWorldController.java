0ef84f4f854e07bb5bf4c1251edafdab508e55c6
pfnie <pfnie@yahoo.com>
2021-09-10 13:05:31 +0800

    jdk1.6

--- servlet3.0-springmvc3.1-helloworld2_1/src/main/java/org/example/controller/HelloWorldController.java
+++ servlet3.0-springmvc3.1-helloworld2_1/src/main/java/org/example/controller/HelloWorldController.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import javax.servlet.http.HttpServletResponse;
-import java.nio.charset.StandardCharsets;
 
 @Controller
 public class HelloWorldController {
@@ -17,7 +16,7 @@ public class HelloWorldController {
 
     @RequestMapping(value = "/v1/hello/world")
     public void hello(@RequestParam String name, HttpServletResponse response) throws Exception{
-        response.getOutputStream().write(helloService.getHelloMessage(name).getBytes(StandardCharsets.UTF_8));
+        response.getOutputStream().write(helloService.getHelloMessage(name).getBytes());
     }
 
 }
