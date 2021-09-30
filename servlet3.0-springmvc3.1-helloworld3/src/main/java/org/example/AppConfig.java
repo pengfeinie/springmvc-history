91af69341ba600bd812017e6b19cb5b489c09fc3
pfnie <pfnie@yahoo.com>
2021-09-30 11:12:08 +0800

    spring

--- servlet3.0-springmvc3.1-helloworld3/src/main/java/org/example/AppConfig.java
+++ servlet3.0-springmvc3.1-helloworld3/src/main/java/org/example/AppConfig.java
@@ -3,7 +3,6 @@ package org.example;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
 @ComponentScan("org.example")
 public class AppConfig {
 	
